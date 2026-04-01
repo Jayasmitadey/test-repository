acfb7770038db555120c30af3f8b307d7be0e1e2
Jayasmita Dey <jayasmitadey01@gmail.com>
2026-04-01 20:36:08 +0530

    todo list project first commit

---
+++ todo.js
@@ -0,0 +1,15 @@
+let tasks=[];
+let input= document.getElementById("inputbox");
+let btn= document.getElementById("add_btn");
+let list= document.getElementById("tasks_list");
+
+btn.addEventListener("click", function() {
+  let value = input.value;
+  tasks.push(value);
+    
+  let div = document.createElement("div");
+  // let div = document.createElement("div");
+  div.innerHTML = value;
+  list.appendChild(div);
+  input.value = "";  
+});
